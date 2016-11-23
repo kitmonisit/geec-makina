8d40e109cddce55d8d2b1c34fbba43d04414d8bd
Monisit, Kristofer <kristofer.monisit@analog.com>
2016-11-23 15:52:41 +0800

    Gather all config variables in one place

---
+++ config/__init__.py
@@ -0,0 +1,18 @@
+DEBUG = True
+
+config_dev = {
+    'MYSQL_USERNAME' : 'username',
+    'MYSQL_PASSWORD' : 'password',
+    'MYSQL_HOST'     : 'localhost',
+    'MYSQL_PORT'     : 3306,
+    'MYSQL_DB'       : 'makina',
+    }
+
+config_prod = {
+    'MYSQL_USERNAME' : 'username',
+    'MYSQL_PASSWORD' : 'password',
+    'MYSQL_HOST'     : 'localhost',
+    'MYSQL_PORT'     : 3306,
+    'MYSQL_DB'       : 'makina',
+    }
+
